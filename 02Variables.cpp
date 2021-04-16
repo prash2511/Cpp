b473e8734cb6d7213099998d93d94bf2e3dddb5b
Prashant Galgunde <76968308+prash2511@users.noreply.github.com>
2021-04-16 09:50:15 +0530

    Add files via upload

---
+++ 02Variables.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	string charName = "Prashant";
+	int charAge = 21;
+	
+	cout<<"There is a student named "<< charName << endl;
+	cout<<"He is "<< charAge <<" years old"<< endl;
+	cout<<"He liked the name "<< charName << endl;
+	cout<<"But did not liked being "<< charAge << endl;
+	
+	
+	return 0;
+}
---
+++ 08MadLibsGame.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	string color, pluralNoun, celebrity;
+	
+	cout<<"Enter color: ";
+	cin>>color;
+	
+	cout<<"Enter Plural Noun: ";
+	cin>>pluralNoun;
+	
+	cout<<"Enter Celebrity Name: ";
+	cin>>celebrity;
+	
+	cout<<endl;
+	cout<<"Roses are "<<color<<endl;
+	cout<<pluralNoun<<" are blue"<<endl;
+	cout<<"I love "<<celebrity<<endl;
+	
+	
+	return 0;
+}
